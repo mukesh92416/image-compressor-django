4f14ae888f660e9d16d4e461736604a37a9b1b5f
Mukesh Kumar Ray <mukeshray995531@gmail.com>
2026-02-01 16:48:55 +0530

    Fix static files config for Render

--- photo_tool/settings.py
+++ photo_tool/settings.py
@@ -118,6 +118,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'editor' / 'static']
+#STATICFILES_DIRS = [BASE_DIR / 'editor' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
