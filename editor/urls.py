01e28c4b17a49ebc19c9654e4db1bde28e04e5b5
Mukesh Kumar Ray <mukeshray995531@gmail.com>
2026-02-05 09:40:45 +0530

    fixed urls structure and adsense meta added

--- editor/urls.py
+++ editor/urls.py
@@ -7,3 +7,11 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
 ]
+from django.http import HttpResponse
+
+def ads_txt(request):
+    return HttpResponse("google.com, pub-789203977629393, DIRECT, f08c47fec0942fa0")
+
+urlpatterns += [
+    path("ads.txt", ads_txt),
+]
